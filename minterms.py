407591b2dafad987b5768b552cb57d954332c4ad
Loochsi <lucas.disarra@gmail.com>
2021-11-19 22:20:05 -0500

    Works, but needs to be cleaned up

--- minterms.py
+++ minterms.py
@@ -1,7 +1,7 @@
 import math
 
 vars = ["X", "Y", "Z", "W", "V", "U"]
-outStr = "0000000011111111"
+outStr = "1011011111101011"
 
 numBits = math.ceil(math.log2(len(outStr)))
 nearestPowOf2 = 2**numBits
@@ -29,23 +29,27 @@ def recursiveDict(bit, bitVals):
         recursiveDict(bit + 1, bitVals)
 
 recursiveDict(0, [0]*numBits)
-print(outDict)
 
 groupings = dict()
 
+# Recursively creates groupings
 def recursiveGroup(varVals):
     numLoops = 0
-    newArr = varVals.copy()
     allTrue = True
-    print(newArr)
     for i in range(len(varVals)):
-        if varVals[i] != 2:
+        newArr = varVals.copy()
+        if newArr[i] != 2:
             continue
         numLoops += 1
         for x in range(2):
             newArr[i] = x
             if not recursiveGroup(newArr):
                 allTrue = False
+    if numLoops == 0:
+        dictStr = ""
+        for i in newArr:
+            dictStr += str(i) 
+        return outDict[dictStr] > 0
 
     numGrouped = 1
     if allTrue:
@@ -59,5 +63,45 @@ def recursiveGroup(varVals):
         groupings[dictStr] = numGrouped
     return allTrue
 
-print(recursiveGroup([2 for i in range(numBits)]))
-print(groupings)
+recursiveGroup([2 for i in range(numBits)])
+
+sortedGroups = []
+nonOverlappingGroups = []
+# SORTING PASS (Sort by Decreasing Grouped bits)
+for i in sorted(groupings, key=groupings.get, reverse=True):
+    sortedGroups.append(i)
+    
+print("ALL GROUPINGS: " + str(sortedGroups))
+
+def aIsRepresentedInB(aSet, bSet):
+    
+    bSubset = True
+    for n in range(len(aSet)-1, -1, -1):
+        if bSet[n] == "2":
+            continue
+        if aSet[n] != bSet[n]:
+            bSubset = False
+    return bSubset
+
+for x in sortedGroups:
+    contained = False
+    for y in nonOverlappingGroups:
+        if aIsRepresentedInB(x, y):
+            contained = True
+    if not contained:
+        nonOverlappingGroups.append(x)
+        
+print("SUPERSET-ONLY GROUPINGS: " + str(nonOverlappingGroups))
+
+equation = ""
+for i in range(len(nonOverlappingGroups)):
+    for n in range(len(nonOverlappingGroups[i])):
+        if nonOverlappingGroups[i][n] == "2":
+            continue
+        if nonOverlappingGroups[i][n] == "1":
+            equation += str(vars[n]) 
+        else:
+            equation += str(vars[n])+"'"
+    if i < len(nonOverlappingGroups)-1:
+        equation += " + "
+print("EQUATION: " + equation)
